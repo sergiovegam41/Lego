44121e51336956739d19c0d456c1ce77c759196a
duvanCode <duvanyepezfa@gmail.com>
2024-12-25 14:40:53 -0500

    add loading

--- assets/js/core/base-lego-framework.js
+++ assets/js/core/base-lego-framework.js
@@ -1,5 +1,6 @@
 import { _loadModulesWithArguments, _loadModules } from "./modules/windows-manager/loads-scripts.js";
 import { activeMenu, toggleSubMenu } from './modules/sidebar/SidebarScrtipt.js';
+import { loading } from './modules/loading/loadingsScript.js';
 // import {_activeMenus, _openModule, _closeModule} from './modules/windows-manager/windows-manager.js'
 
 window.lego = window.lego || {};
@@ -13,4 +14,20 @@ window.toggleSubMenu = toggleSubMenu;
 // lego.activeMenus = _activeMenus;
 
 activeMenu()
+
+
+
+
+/*
+loading(true,{
+    withMenu:true
+});
+
+loading(false,{
+    success:true,
+    message:"ok"
+});
+*/
+
+
 // _activeMenus()
---
+++ assets/js/core/modules/loading/loadingComponents.js
@@ -0,0 +1,27 @@
+const createElementLoading = ( feedbackObject = {} ) => {
+    let overlay = document.createElement('DIV');
+    overlay.style.position = 'absolute';
+    overlay.style.height = '100%';
+    overlay.style.width = '100%';
+    overlay.style.background = 'rgb(255 255 255 / 17%)';
+    overlay.style.top = '0';
+    overlay.style['backdrop-filter'] = 'blur(1.5px)';
+    overlay.style['display'] = 'flex';
+    overlay.style['justify-content'] = 'center';
+    overlay.style['align-items'] = 'center';
+    overlay.style['flex-direction'] = 'column';
+
+    if(feedbackObject?.withMenu) overlay.style['z-index'] = '100';
+    overlay.id = 'loadingOverlay';
+
+    return overlay;
+}; 
+
+const createElementImgLoading = ( feedbackObject = {} ) => {
+    let loadingImage = document.createElement('img');
+    loadingImage.src = 'assets/images/loading/loading.svg';
+    loadingImage.classList.add('spinner');
+    return loadingImage;
+};
+
+export { createElementLoading, createElementImgLoading };
