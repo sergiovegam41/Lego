821efab3ce3c4c8e62302737f022af1c87337f4d
sergio <59804620+sergiovegam41@users.noreply.github.com>
2025-09-14 17:11:57 -0500

    Clean

--- public/assets/js/core/base-lego-login.js
+++
@@ -1,9 +0,0 @@
-import { _loadModulesWithArguments, _loadModules } from "./modules/windows-manager/loads-scripts.js";
-import { _openModule, _closeModule} from './modules/windows-manager/windows-manager.js'
-import { loading } from './modules/loading/loadingsScript.js';
-
-window.lego = window.lego || {};
-
-lego.loadModulesWithArguments = _loadModulesWithArguments;
-lego.loadModules = _loadModules;
-lego.loading = loading;
--- public/assets/js/home/home.js
+++
@@ -1,4 +0,0 @@
-let context = {CONTEXT}
-// console.log("Home console")
-
-// console.log(context)
